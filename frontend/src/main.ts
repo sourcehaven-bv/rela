f349dc1072b78d1992ff890be61927183991fab4
Jeroen Vloothuis <jvloothuis@sourcehaven.nl>
2026-05-09 16:15:45 +0200

    feat: Sourcehaven brand theme (light + dark default)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import '@fontsource/open-sans/400.css'
+import '@fontsource/open-sans/500.css'
+import '@fontsource/open-sans/600.css'
+import '@fontsource/open-sans/700.css'
 import './styles/back-button.css'
 
 const app = createApp(App)
